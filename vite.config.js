71e103eafac3aecf5ce7db6881fa2291b9349bd2
Karan09542 <Karan09542@gamil.com>
2025-02-01 15:51:33 +0530

    महादेव understand sameSiteSecure(sSS)

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,16 @@ import { VitePWA } from "vite-plugin-pwa";
 export default defineConfig({
   plugins: [react(), svgr()],
   server: {
+    // proxy: {
+    //   "/user": "http://localhost:8000",
+    //   "/question": "http://localhost:8000",
+    //   "/post": "http://localhost:8000",
+    //   "/report": "http://localhost:8000",
+    //   "/preference": "http://localhost:8000",
+    //   "/search-result": "http://localhost:8000",
+    //   "/book-mark": "http://localhost:8000",
+    //   "/comment": "http://localhost:8000",
+    // },
     proxy: {
       "/user": "https://quora-backend-rosy.vercel.app",
       "/question": "https://quora-backend-rosy.vercel.app",
